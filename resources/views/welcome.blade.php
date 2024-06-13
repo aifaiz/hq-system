a3e622852bbc2a153c684eb2832117e70bc1b582
FAiZ <ait.mizul@gmail.com>
2024-06-13 15:00:15 +0800

    front coming soon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <span class="text-xl font-semibold text-black dark:text-white">HQ System</span>
+                            <span class="text-xl font-semibold text-black dark:text-white">&nbsp;</span>
                         </div>
                     </header>
 
