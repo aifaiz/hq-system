7ae25dcd77c88e05777f47cf0c561dad571ac897
FAiZ <ait.mizul@gmail.com>
2024-07-01 18:22:38 +0800

    latest view agent shop

--- app/Filament/Distributor/Resources/AgentUserResource/Pages/ViewAgent.php
+++ app/Filament/Distributor/Resources/AgentUserResource/Pages/ViewAgent.php
@@ -3,10 +3,25 @@
 namespace App\Filament\Distributor\Resources\AgentUserResource\Pages;
 
 use App\Filament\Distributor\Resources\AgentUserResource;
+use App\Models\AgentUser;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewAgent extends ViewRecord
 {
     protected static string $resource = AgentUserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // Actions\CreateAction::make(),
+            Actions\Action::make('View Shop')
+                ->color('info')
+                ->icon('heroicon-o-globe-alt')
+                ->url(function(AgentUser $record){
+                    return route('agent.product', ['refcode'=>$record->refcode]);
+                })
+                ->openUrlInNewTab()
+        ];
+    }
 }
