2e1bcdd1b8f4747a22e545596bfc52fd7c2de698
FAiZ <ait.mizul@gmail.com>
2024-07-01 23:29:58 +0800

    style update

--- app/Livewire/Agent/ViewProduct.php
+++ app/Livewire/Agent/ViewProduct.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire\Agent;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ViewProduct extends Component
 {
+    public $refcode;
+    public $slug;
+    public $product;
+
+    public function mount($refcode,$slug)
+    {
+        $this->refcode = $refcode;
+        $this->slug = $slug;
+        $product = Product::where('slug', $slug)->first();
+        if(!$product):
+            abort(404);
+        endif;
+
+        $this->product = $product;
+    }
     public function render()
     {
         return view('livewire.agent.view-product');
